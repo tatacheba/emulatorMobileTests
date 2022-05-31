a9bbd62e070917bba1d4ee9145ddf4131224943c
TatianaCheb <tanya.cheba12@gmail.com>
2022-05-31 14:12:19 +0300

    Update Credentials.java

--- src/test/java/config/Credentials.java
+++ src/test/java/config/Credentials.java
@@ -1,8 +1,18 @@
 package config;
+
 import org.aeonbits.owner.ConfigFactory;
 
-
 public class Credentials {
-    public static EmulatorConfig configEmulator = ConfigFactory.create(EmulatorConfig.class, System.getProperties());
-    public static BrowserstackConfig configBrowserstack = ConfigFactory.create(BrowserstackConfig.class);
+
+    public static CredentialsConfig config = ConfigFactory.create( CredentialsConfig.class);
+
+    public static boolean isBrowserStack() {
+        return System.getProperty("device").equals("browserstack");
+    }
+    public static String deviceTest() {
+        if(System.getProperty("device").equals("real")){
+            return config.deviceNameReal();
+        }
+        return config.deviceName();
+    }
 }
