28b1b9070ea6ca5ec001d95b8b16b4731ed1b45e
TatianaCheb <tanya.cheba12@gmail.com>
2022-05-31 14:12:27 +0300

    Update EmulatorMobileDriver.java

--- src/test/java/drivers/EmulatorMobileDriver.java
+++ src/test/java/drivers/EmulatorMobileDriver.java
@@ -16,7 +16,6 @@ import java.net.URL;
 
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
-
 public class EmulatorMobileDriver implements WebDriverProvider {
 
     @Override
@@ -26,16 +25,32 @@ public class EmulatorMobileDriver implements WebDriverProvider {
         UiAutomator2Options options = new UiAutomator2Options();
         options.merge(capabilities);
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setPlatformName(Credentials.configEmulator.platformName());
-        options.setDeviceName(Credentials.configEmulator.deviceName());
-        options.setPlatformVersion(Credentials.configEmulator.platformVersion());
-        options.setApp(app.getAbsolutePath());
-        options.setLocale("en");
+        options.setPlatformName(Credentials.config.platformName());
+        options.setDeviceName(Credentials.deviceTest());
+//        options.setPlatformVersion("11.0");
         options.setLanguage("en");
-        options.setAppPackage("org.wikipedia.alpha");
-        options.setAppActivity("org.wikipedia.main.MainActivity");
-
+        options.setLocale("en");
+        options.setApp(app.getAbsolutePath());
+//        Явное описание пакета и Активности
+        options.setAppPackage(Credentials.config.appPackage());
+        options.setAppActivity(Credentials.config.appActivity());
+//
         return new AndroidDriver(getAppiumServerUrl(), options);
+    }
+    private File getApp() {
+        String appPath = "src/test/resources/apk/app-alpha-universal-release.apk";
+        String appUrl = "https://github.com/wikimedia/apps-android-wikipedia/" +
+                "releases/download/latest/app-alpha-universal-release.apk?raw=true";
+        File app = new File(appPath);
+        if (!app.exists()) {
+            String url = appUrl;
+            try (InputStream in = new URL(url).openStream()) {
+                copyInputStreamToFile(in, app);
+            } catch (IOException e) {
+                throw new AssertionError(" Failed download apk");
+            }
+        }
+        return app;
     }
 
     public static URL getAppiumServerUrl() {
@@ -45,21 +60,4 @@ public class EmulatorMobileDriver implements WebDriverProvider {
             throw new RuntimeException(e);
         }
     }
-
-    private File getApp() {
-        String appPath = "src/test/resources/apk/app-alpha-universal-release.apk";
-        String appUrl = "https://github.com/wikimedia/apps-android-wikipedia/" +
-                "releases/download/latest/app-alpha-universal-release.apk?raw=true";
-
-        File app = new File(appPath);
-        if (!app.exists()) {
-            try (InputStream in = new URL(appUrl).openStream()) {
-                copyInputStreamToFile(in, app);
-            } catch (IOException e) {
-                throw new AssertionError("Failed to download apk", e);
-            }
-        }
-        return app;
-    }
-
 }
