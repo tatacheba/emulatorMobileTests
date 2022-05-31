e1fedd62f19ad40a03fad7c51636efffaa513a79
TatianaCheb <tanya.cheba12@gmail.com>
2022-05-31 14:12:33 +0300

    Update Browserstack.java

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,18 +1,25 @@
 package helpers;
 
-import config.Credentials;
-
 import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 
 public class Browserstack {
+    // todo hide with Owner
+    // DO NOT STORE CREDENTIALS, ONLY FOR EXAMPLE
+    public static String
+            browserstackLogin = "tpuretest_lSVWc2",
+            browserstackPassword = "Ggd5nW19cyeGseGGn2m6";
 
     public static String videoUrl(String sessionId) {
+        String url = format("https://api-cloud.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic(Credentials.configBrowserstack.user(), Credentials.configBrowserstack.key())
+                .auth().basic(browserstackLogin, browserstackPassword)
+                .log().all()
                 .when()
-                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
+                .get(url)
                 .then()
+                .log().all()
                 .statusCode(200)
                 .extract()
                 .path("automation_session.video_url");
